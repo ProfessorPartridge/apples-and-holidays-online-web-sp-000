3aa89f3f261dcc1f7b77a9c2b094abed1e89304f
Josiah  Partridge <jpartridj94@gmail.com>
2020-09-09 20:52:26 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -67,7 +67,7 @@ binding.pry
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-
+  holiday_hash[]
 end
 
 def all_supplies_in_holidays(holiday_hash)
