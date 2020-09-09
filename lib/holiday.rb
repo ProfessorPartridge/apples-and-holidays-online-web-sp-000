2fe087ca53229914ea9146f92016a5230e331c74
Josiah  Partridge <jpartridj94@gmail.com>
2020-09-09 21:19:25 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -75,7 +75,14 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+  holiday_hash.each do |season, holiday|{
+    puts "#{season.to_s.capitalize}:"
+    holiday.each do |name, supplies|{
+      capitalized_name = name.to_s.split("_").each do |word|
+        word.capitalize!
+        
+    }
+  }
 
 end
 
