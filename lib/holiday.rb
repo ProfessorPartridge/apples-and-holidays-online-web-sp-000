e79d6617fa88c0a12bc6cee27d3abd4f7172dcda
Josiah  Partridge <jpartridj94@gmail.com>
2020-09-09 20:29:47 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -1,4 +1,19 @@
 require 'pry'
+holiday_hash = {
+     :winter => {
+       :christmas => ["Lights", "Wreath"],
+       :new_years => ["Party Hats"]
+     },
+     :summer => {
+       :fourth_of_july => ["Fireworks", "BBQ"]
+     },
+     :fall => {
+       :thanksgiving => ["Turkey"]
+     },
+     :spring => {
+       :memorial_day => ["BBQ"]
+     }
+   }
 
 def second_supply_for_fourth_of_july(holiday_hash)
   # given that holiday_hash looks like this:
@@ -18,21 +33,28 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+  holiday_hash[:summer][:fourth_of_july][1]
 end
 
-def add_supply_to_winter_holidays(holiday_hash, supply)
+def add_supply_to_winter_holidays(hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
 
+  hash[:winter][:christmas].push(supply)
+  hash[:winter][:new_years].push(supply)
 end
+#add_supply_to_winter_holidays(holiday_hash, 'Test')
 
 
-def add_supply_to_memorial_day(holiday_hash, supply)
+def add_supply_to_memorial_day(hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
-
+  hash[:spring][:memorial_day].push(supply)
+  
 end
+add_supply_to_memorial_day(holiday_hash, 'Test')
+binding.pry
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
